a1ab32b77098251e9210fe92ef805819416b14e1
ElinaKl <lilo.puf@yandex.ru>
2025-04-18 13:23:34 +0300

    add config loading

--- jiraConnector/cmd/service/main.go
+++ jiraConnector/cmd/service/main.go
@@ -1,11 +1,32 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	config "github.com/jiraconnector/internal/configReader"
+)
+
 func main() {
 	//Open config
+	cfgPath := "../../configs/config.yml" // dev
+	configFile, err := os.Open(cfgPath)
+	if err != nil {
+		log.Println("error open config")
+		panic(err)
+	}
+	log.Println("open config")
 
 	//load data from config
+	cfg, err := config.LoadConfig(configFile)
+	if err != nil {
+		log.Println("error load config")
+		panic(err)
+	}
+	log.Println("load config")
 
 	//setting logger
+	log.Printf("set logger") //TEMP
 
 	//create connector app
 
