3c6fc946637d4a4938ce94fc34ec5b583a21c11b
ElinaKl <lilo.puf@yandex.ru>
2025-04-18 13:18:31 +0300

    init jiraConnector

---
+++ jiraConnector/cmd/service/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	//Open config
+
+	//load data from config
+
+	//setting logger
+
+	//create connector app
+
+	//start app
+}
---
+++ jiraConnector/go.mod
@@ -0,0 +1,3 @@
+module github.com/jiraconnector
+
+go 1.24.2
